7d3ed978896d7ea32990a6ba672c4086155a3e53
LastGuardian89 <dmitriy-bobrovskiy03@mai.ru>
2021-12-05 12:09:06 +0300

    test ver 0.1

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,10 +1,10 @@
+//Copyright by Enigma
 #include <header.hpp>
 #include <iostream>
 
 
 int main() {
-  // path  менять в зависимости от системы
-  std::string path("/home/enigma/twinLabs/c++ developer/lab 4/lab-04-boost-filesystem/misc/ftp");
+  std::string path("../misc/ftp");
   Filesystem test(path);
   std::cout << test;
 }
